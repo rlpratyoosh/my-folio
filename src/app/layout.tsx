9ff41c8f9730ae4618a7d968dcad9308d8fdae79
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-09-21 13:47:26 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,48 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Pratyoosh - A fullstack bugger",
-  description: "I bug more than you can imagine.",
-  icons: {
-    icon: "/favicon.png",
-  },
+    title: "Pratyoosh - A fullstack bugger",
+    description: "I bug more than you can imagine.",
+    icons: {
+        icon: "/favicon.png",
+    },
+    openGraph: {
+        type: "website",
+        url: "https://my-folio-khaki-six.vercel.app/",
+        title: "Pratyoosh - A fullstack bugger",
+        description: "I bug more than you can imagine.",
+        siteName: "Pratyoosh Portfolio",
+        images: [
+            {
+                url: "/thumbnail.png",
+                width: 1200,
+                height: 630,
+                alt: "Pratyoosh Portfolio",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Pratyoosh - A fullstack bugger",
+        description: "I bug more than you can imagine.",
+        images: ["/thumbnail.png"],
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="dark">
-        <SessionProvider>
-          <main>{children}</main>
-        </SessionProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="dark">
+                <SessionProvider>
+                    <main>{children}</main>
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
